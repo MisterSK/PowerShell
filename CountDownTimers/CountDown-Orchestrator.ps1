9186e49e4c7294cd69574ab37b0166784bf68618
Sanya Khasenye <MisterSK@users.noreply.github.com>
2023-08-15 07:36:40 +0100

    Add CountDownTimers script folder

---
+++ CountDownTimers/CountDown-Orchestrator.ps1
@@ -0,0 +1,3 @@
+cd C:\_SCRIPTS_DO_NOT_DELETE\PowerShell\CountDownTimers\; Start-Sleep 20; C:\_SCRIPTS_DO_NOT_DEL
+ETE\MS_DOS\TestForShutdown.bat; .\Start-Countdown.ps1 -Seconds 10800 -Message 'Waiting for countdown timer...'; powershe
+ll.exe C:\_SCRIPTS_DO_NOT_DELETE\ScheduledTasks\update-cancel-shutdown-task.ps1 -Time "2021-02-23T20:30:30Z"
