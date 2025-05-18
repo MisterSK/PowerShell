68f17fffdc656bfb17717a2d0fac6eac8e29dfdb
Sanya Khasenye <MisterSK@users.noreply.github.com>
2025-05-18 12:38:36 +0100

    Adding alternative, temp version of a working window manager

---
+++ PwshShellWindowManager/_PowerShellWindowManager.ps1
@@ -0,0 +1,14 @@
+# Open 8 PowerShell windows with black background and light green text (using color command)
+1..8 | ForEach-Object {
+    # Adjust the min and max values on each run
+    $minvalue = Get-Random -Minimum 1 -Maximum 499
+    $maxvalue = Get-Random -Minimum 999 -Maximum 499000
+    $command = @"
+        cmd /c color 0A
+        mode con: cols=80 lines=10
+        powershell.exe -file 'C:\Users\SanyaKhasenye(Sensit\WorkingDir\PowerShell\CountDownTimers\Start-Countdown.ps1' -seconds (Get-Random -Minimum $minvalue -Maximum $maxvalue) -message 'Waiting to launch Activity Orchestrator'
+        Set-Location 'C:\Users\SanyaKhasenye(Sensit\WorkingDir\PowerShell\ActivityGenerator.v3'
+        .\Start-ActivityOrchestrator_v0.1.ps1
+"@
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", $command
+}
